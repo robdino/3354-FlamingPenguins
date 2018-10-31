ad712b073ef71ae6ac55e92cb8dff366c2949f27
Robbie Castillo <jaygrazer@gmail.com>
2018-10-30 21:31:35 -0500

    First Prototype Build
    This is the first prototype of the calendar. Barebones stuff using CalendarView widget and Intents.

---
+++ Code/app/src/main/java/com/example/robcastle/flamingcalendar/CalendarActivity.java
@@ -0,0 +1,38 @@
+package com.example.robcastle.flamingcalendar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.content.*;
+
+//This class generates the calendar itself
+public class CalendarActivity extends AppCompatActivity
+{
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
+
+    //NOTE: this creates the calendar activity itself
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+
+        //NOTE: This is for when we select a new date in the calendar (the pink highlight)
+        //We go back to our main page (HomeScreen) to show the date and any pertinent info.
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth)
+            {
+                String date = (month + 1) + "/" + dayOfMonth + "/" + year;
+                Intent intent2 = new Intent(CalendarActivity.this, HomeScreen.class);
+                intent2.putExtra("date", date);
+                startActivity(intent2);
+            }
+        });
+    }
+}
--- Code/app/src/main/java/com/example/robcastle/flamingcalendar/HomeScreen.java
+++ Code/app/src/main/java/com/example/robcastle/flamingcalendar/HomeScreen.java
@@ -1,13 +1,50 @@
 package com.example.robcastle.flamingcalendar;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle;;
+import android.view.View;
+import android.widget.*;
+import android.content.*;
 
+
+/**
+*@author Robbie Castillo
+*@since 10/30/18
+* This HomeScreen class generates the default page for the application.
+* First it creates the home screen.
+* If the "Go to Calendar" button is clicked, it goes to a CalendarView
+* The user can select a date, and the app will return to this default screen.
+* This time however, the default screen will display the date selected (ex. 10/31/18)
+*
+ */
 public class HomeScreen extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    public TextView date;
+    private Button btnGoToCalendar;
+
+    //create base app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        //generate button for calendar and date text
+        date = (TextView) findViewById(R.id.date);
+        btnGoToCalendar = (Button) findViewById(R.id.btnGoToCalendar);
+
+        //Get info from calendar button click & display the date clicked
+        Intent incomingIntent1 = getIntent();
+        String incDate1 = incomingIntent1.getStringExtra("date");
+        date.setText(incDate1);
+
+        //handles the "Go To Calendar" button
+        btnGoToCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(HomeScreen.this, CalendarActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
+
 }
