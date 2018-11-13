a446cccbcd5a9ec0c3ee6746c211f4fd7ed61c55
Taylor Kettle <tmkettle23xbox@gmail.com>
2018-11-13 12:10:02 -0600

    Update Code/app/src/androidTest/java/com/example/robcastle/flamingcalendar/TestFpEventTest.java

--- Code/app/src/androidTest/java/com/example/robcastle/flamingcalendar/TestFpEventTest.java
+++ Code/app/src/androidTest/java/com/example/robcastle/flamingcalendar/TestFpEventTest.java
@@ -3,7 +3,7 @@ package com.example.robcastle.flamingcalendar;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+//Figuring out Junit testing with the FpEvents class
 public class TestFpEventTest {
     fpEvent myUnit = new fpEvent("10/29/1994", "Birthday", "12:00am", "12:00pm");
 
