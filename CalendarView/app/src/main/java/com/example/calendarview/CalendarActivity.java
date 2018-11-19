cad8dfaae4e91748866b40cd225eed201d4d7bb5
Taylor Kettle <tmkettle23xbox@gmail.com>
2018-11-18 22:51:09 -0600

    implementing the select date wheel

---
+++ CalendarView/app/src/main/java/com/example/calendarview/CalendarActivity.java
@@ -0,0 +1,29 @@
+package com.example.calendarview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange( CalendarView calendarView, int i, int i1, int i2) {
+                        String date = (i1+1) + "/" + i2 + "/" + i;
+                Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy: " + date);
+                Intent intent  = new Intent(CalendarActivity.this, MainActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
+    }
+}
