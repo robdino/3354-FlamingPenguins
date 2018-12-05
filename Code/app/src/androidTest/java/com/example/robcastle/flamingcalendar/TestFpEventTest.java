b25ff6a9f2cef70a7888e56aa3843fb2ca026b9b
Taylor Kettle <tmkettle23xbox@gmail.com>
2018-12-04 19:09:25 -0600

    More Test Cases for fpEvents Class
    Testing set Methods

--- Code/app/src/androidTest/java/com/example/robcastle/flamingcalendar/TestFpEventTest.java
+++ Code/app/src/androidTest/java/com/example/robcastle/flamingcalendar/TestFpEventTest.java
@@ -17,31 +17,60 @@ public class TestFpEventTest {
         assertEquals("10/29/1994", result);
     }
     @Test
+    public void testSetDate() {
+        myUnit.setDate("11/1/2018");
+        String result = myUnit.getDate();
+        assertEquals("11/1/2018", result);
+    }
+
+    @Test
     public void testgetDescription() {
         String result = myUnit.getDescription();
         assertEquals("Birthday", result);
     }
+    @Test
+    public void testsetDescription() {
+        myUnit.setDescription("Manual Code Review");
+        String result = myUnit.getDescription();
+        assertEquals("Manual Code Review", result);
+    }
+
     @Test
     public void testgetStartTime() {
         String result = myUnit.getStartTime();
         assertEquals("12:00am", result);
     }
     @Test
+    public void testsetStartTime() {
+        myUnit.setStartTime("3:00pm");
+        String result = myUnit.getStartTime();
+        assertEquals("2:00pm", result);
+    }
+
+    @Test
     public void testgetEndTime() {
         String result = myUnit.getEndTime();
         assertEquals("12:00pm", result);
     }
+    @Test
+    public void testsetEndTime() {
+        myUnit.setEndTime("2:00pm");
+        String result = myUnit.getEndTime();
+        assertEquals("2:00pm", result);
+    }
+
     @Test
     public void testgetName() {
         String result = myUnit.getName();
         assertEquals("Party", result);
     }
     @Test
-    public void testSetDate() {
-        myUnit.setDate("11/1/2018");
-        String result = myUnit.getDate();
-        assertEquals("11/1/2018", result);
+    public void testsetName() {
+        myUnit.setName("Company Meeting");
+        String result = myUnit.getName();
+        assertEquals("Company Meeting", result);
     }
+
 
 
 
